3c8fe04c2751576c380098e07a5ce0543fc156bf
jakub41 <lemiszewski@gmx.com>
2020-02-04 17:27:40 +0100

    Building profile page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+// Components
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <>
-      <h1>HELLO</h1>
-      {/* <Router basename="/">
+
+        <NavBar />
+        {/* <Router basename="/">
 
       </Router> */}
+
     </>
   );
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,41 +1,58 @@
 import React from "react";
-import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+  InputGroup
+} from "react-bootstrap";
+import Logo from "../../li-logo.png";
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 function NavBar() {
   return (
     <>
-      <Navbar bg="light">
-        <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-      </Navbar>
-      <br />
-      <Navbar bg="light">
-        <Navbar.Brand>Brand text</Navbar.Brand>
-      </Navbar>
-      <br />
-      <Navbar bg="dark">
-        <Navbar.Brand href="#home">
-          <img
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-      </Navbar>
-      <br />
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          React Bootstrap
-        </Navbar.Brand>
+      <Navbar
+        collapseOnSelect
+        sticky="top"
+        className="nav-li-bg justify-content-between"
+        variant="light"
+      >
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={Logo}
+              width="50"
+              height="50"
+              className="d-inline-block align-top li-logo-bg"
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Form inline>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">
+                  <FaSearch />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Form>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
