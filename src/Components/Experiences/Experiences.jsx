ad020d3671c8c146466363d50ec363b1762dc727
jakub41 <lemiszewski@gmx.com>
2020-02-05 16:28:49 +0100

    Expiriences added a view

--- src/Components/Experiences/Experiences.jsx
+++ src/Components/Experiences/Experiences.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Container, Image } from "react-bootstrap";
+import { Row, Col, Container, Image, Card, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { ExperiencesMiddleware } from "../../Store/Middleware";
 import { USERNAME } from "../../Services/constAPI";
@@ -16,24 +16,39 @@ const Experiences = () => {
     dispatch(ExperiencesMiddleware.getAllExperiencesProfile(USERNAME));
   }, [dispatch]);
 
-  return (
+  return !experiences.object ? (
     <>
       <Container>
         <h1>Experience</h1>
-
         {experiences.profileExperiences &&
           experiences.profileExperiences
             .map(P => P.experience)
             .flat()
             .map((E, k) => (
               <Row key={k}>
-                <ul>
-                  <li>{E.role}</li>
-                </ul>
+                <Col md={12}>
+                  <Card style={{ width: "100%" }} className="mb-3">
+                    <Row className="no-gutters">
+                      <Col md={4}>
+                        <Image src={E.image} alt="exp img" />
+                      </Col>
+                      <Col md={8}>
+                        <Card.Body>
+                          <Card.Title>{E.role}</Card.Title>
+                          <Card.Text>{E.company}</Card.Text>
+                          <Card.Text>{E.area}</Card.Text>
+                          <Card.Text>{E.startDate}</Card.Text>
+                        </Card.Body>
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
               </Row>
             ))}
       </Container>
     </>
+  ) : (
+    <h3 className="red-text mt-5">No experiences to show</h3>
   );
 };
 
